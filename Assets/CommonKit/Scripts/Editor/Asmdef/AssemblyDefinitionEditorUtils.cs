323087aee867c0431797be4cff8d2d418e891ca4
JunYu <>
2025-08-20 10:04:25 +0800

    feat: add UniGears.CommonKit.Editor

---
+++ Assets/CommonKit/Scripts/Editor/Asmdef/AssemblyDefinitionEditorUtils.cs
@@ -0,0 +1,103 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEditorInternal;
+
+namespace UniGears.CommonKit.Editor
+{
+    internal static class AssemblyDefinitionEditorUtils
+    {
+        private const string SORT_ALL_ASMDEF_REFERENCES_IN_ASSETS = "UniGears/Common/Asmdef/Sort All Asmdef References In Assets";
+        private const string SORT_ALL_ASMDEF_REFERENCES_RECURSIVELY = "Assets/UniGears/Common/Asmdef/Sort All Asmdef References Recursively";
+        private const string SORT_ASMDEF_REFERENCES = "Assets/UniGears/Common/Asmdef/Sort Asmdef References";
+        private const string SORT_COMPLETED = "Completed";
+
+        [MenuItem(SORT_ALL_ASMDEF_REFERENCES_IN_ASSETS, false, 0)]
+        private static void SortAllAsmdefReferencesInAssets()
+        {
+            var allAssemblyDefinitionAssetPath = AssetDatabase
+                    .GetAllAssetPaths()
+                    .Where(x => x.EndsWith(".asmdef") && x.StartsWith("Assets/"))
+                    .Distinct()
+                    .ToArray();
+
+            SortGroup(allAssemblyDefinitionAssetPath);
+        }
+
+        [MenuItem(SORT_ALL_ASMDEF_REFERENCES_RECURSIVELY, true)]
+        private static bool ValidateSortAllAsmdefReferencesRecursively()
+        {
+            return Selection.objects.OfType<DefaultAsset>().Any(x => AssetDatabase.IsValidFolder(AssetDatabase.GetAssetPath(x)));
+        }
+
+        [MenuItem(SORT_ALL_ASMDEF_REFERENCES_RECURSIVELY, false, 1)]
+        private static void SortAllAsmdefReferencesRecursively()
+        {
+            var selectedPaths = Selection.assetGUIDs
+                    .Select(AssetDatabase.GUIDToAssetPath)
+                    .Where(AssetDatabase.IsValidFolder)
+                    .ToArray();
+
+            var assemblyDefinitionAssetArray = selectedPaths
+                    .SelectMany(x => AssetDatabase.FindAssets("t:AssemblyDefinitionAsset", new[] { x }))
+                    .Select(AssetDatabase.GUIDToAssetPath)
+                    .Where(x => x.EndsWith(".asmdef") && x.StartsWith("Assets/"))
+                    .Distinct()
+                    .ToArray();
+
+            SortGroup(assemblyDefinitionAssetArray);
+        }
+
+        [MenuItem(SORT_ASMDEF_REFERENCES, true)]
+        private static bool ValidateSortAsmdefReferences()
+        {
+            return Selection.objects.OfType<AssemblyDefinitionAsset>().Any();
+        }
+
+        [MenuItem(SORT_ASMDEF_REFERENCES, false, 2)]
+        private static void SortAsmdefReferences()
+        {
+            var assemblyDefinitionAssetArray = Selection.objects
+                    .OfType<AssemblyDefinitionAsset>()
+                    .Select(x => AssetDatabase.GetAssetPath(x))
+                    .Where(x => x.EndsWith(".asmdef") && x.StartsWith("Assets/"))
+                    .Distinct()
+                    .ToArray();
+
+            SortGroup(assemblyDefinitionAssetArray);
+        }
+
+        private static void SortGroup(string[] assetPaths)
+        {
+            try
+            {
+                AssetDatabase.StartAssetEditing();
+
+                var length = assetPaths.Length;
+
+                for (var i = 0; i < length; i++)
+                {
+                    var number = i + 1;
+                    var assetPath = assetPaths[i];
+
+                    EditorUtility.DisplayProgressBar
+                    (
+                        title: "Sort Assembly Definition References",
+                        info: $"{number} / {length}    {assetPath}",
+                        progress: (float)number / length
+                    );
+
+                    AssemblyDefinitionReferencesSorter.Sort(assetPath);
+                }
+            }
+            finally
+            {
+                EditorUtility.ClearProgressBar();
+                AssetDatabase.StopAssetEditing();
+            }
+
+            AssetDatabase.Refresh();
+
+            EditorUtility.DisplayDialog("", $"{SORT_COMPLETED}", "OK");
+        }
+    }
+}
---
+++ Assets/CommonKit/Scripts/Editor/Asmdef/AssemblyDefinitionJson.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace UniGears.CommonKit.Editor
+{
+    [Serializable]
+    public sealed class AssemblyDefinitionJson
+    {
+        public string name = string.Empty;
+        public string rootNamespace = string.Empty;
+        public string[] references = Array.Empty<string>();
+        public string[] includePlatforms = Array.Empty<string>();
+        public string[] excludePlatforms = Array.Empty<string>();
+        public bool allowUnsafeCode;
+        public bool overrideReferences;
+        public string[] precompiledReferences = Array.Empty<string>();
+        public bool autoReferenced;
+        public string[] defineConstraints = Array.Empty<string>();
+        public string[] versionDefines = Array.Empty<string>();
+        public bool noEngineReferences;
+    }
+}
